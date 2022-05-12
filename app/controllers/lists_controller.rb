d470a3e0e7dcf7ca4634818324a2f3174a265549
luis rodriguez <rodluisr@gmail.com>
2022-05-12 16:30:32 +0900

    functional

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = @list.bookmarks
+    @movies = Movie.all
+  end
+
+  def new
+    @list = List.new
+    # @movies = Movies.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
